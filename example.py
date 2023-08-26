2a5c049453e8c0951986994c520f6eaba5e0cb58
Choi Minkyu (mc76728) <mc76728@ece-895237.austin.utexas.edu>
2023-08-26 16:30:47 -0500

    update: pyproject.toml

--- example.py
+++ example.py
@@ -1,7 +1,2 @@
-def main():
-    """Provide example code to run your code / application."""
-    print("You're all set")
-
-
 if __name__ == "__main__":
-    main()
+    print("Development is in progress.")
--- your_project_name/subpackage_name/subpackage_module_name.py
+++
@@ -1,1 +0,0 @@
-"""subpackage module."""
