fdee7372ff13b37f1aa027647a02814f75a899c4
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-08-29 19:29:09 -0500

    update: remove git submodule

--- install.sh
+++ install.sh
@@ -8,21 +8,20 @@ echo "Downloading GroundingDINO & Segment Anything Model (SAM) Weights"
 WEIGHT_DIR=$ROOT_DIR/artifacts/weights
 if [[ ! -e $WEIGHT_DIR ]]; then
     mkdir -p $WEIGHT_DIR
+    cd $WEIGHT_DIR
+    wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
+    wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 elif [[ ! -d $WEIGHT_DIR ]]; then
     echo "$WEIGHT_DIR already exists but is not a directory" 1>&2
 fi
 
-cd $WEIGHT_DIR
-wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
-wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+# # -- Installing GroundingDINO -- #
+# echo "Installing GroundingDINO"
+# git submodule init
+# git submodule update
 
-# -- Installing GroundingDINO -- #
-echo "Installing GroundingDINO"
-git submodule init
-git submodule update
-
-cd $ROOT_DIR/video_to_automaton/GroundingDINO
-git checkout -q 57535c5a79791cb76e36fdb64975271354f10251
+# cd $ROOT_DIR/video_to_automaton/GroundingDINO
+# git checkout -q 57535c5a79791cb76e36fdb64975271354f10251
 
 # -- Installing video_to_automaton -- #
 echo "Installing video_to_automaton"
