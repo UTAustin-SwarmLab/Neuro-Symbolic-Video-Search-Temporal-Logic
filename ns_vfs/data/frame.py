3bfcdf6fe09a875095d8ccc6abba9e50f4863755
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-09-20 19:30:37 -0500

    feat: data generation framework

--- ns_vfs/data/frame.py
+++ ns_vfs/data/frame.py
@@ -1,4 +1,6 @@
-import dataclasses
+import dataclasses  # noqa: D100
+import random
+from typing import List, Optional
 
 import numpy as np
 
@@ -24,6 +26,7 @@ class FrameWindow:
     verification_result: bool = False
 
     def get_propositional_confidence(self):
+        """Get propositional confidence."""
         propositional_confidence = [
             [] for i in range(len(self.frame_image_set[0].propositional_probability.keys()))
         ]
@@ -38,5 +41,97 @@ class FrameWindow:
         return propositional_confidence
 
     def update_states(self, states):
+        """Update states."""
         self.states = states
         return states
+
+
+@dataclasses.dataclass
+class BenchmarkRawImage:
+    """Benchmark image frame class."""
+
+    unique_labels: list
+    labels: List[str]
+    images: List[np.ndarray]
+
+    def sample_image_from_label(self, labels: list, proposition: list) -> np.ndarray:
+        """Sample image from label."""
+        image_of_frame = []
+        img_to_label = {}
+        for prop in proposition:
+            img_to_label[prop] = [i for i, value in enumerate(self.labels) if value == prop]
+
+        label_idx = 0
+        for lable in labels:
+            if lable is None:
+                while True:
+                    random_idx = random.randrange(len(self.images))
+                    if self.labels[random_idx] not in proposition:
+                        break
+                labels[label_idx] = self.labels[random_idx]
+                image_of_frame.append(self.images[random_idx])
+            else:
+                random_idx = random.choice(img_to_label[lable])
+                image_of_frame.append(self.images[random_idx])
+
+            label_idx += 1
+        return labels, image_of_frame
+
+
+@dataclasses.dataclass
+class BenchmarkLTLFrame:
+    """Benchmark image frame class.
+
+    ground_truth (bool): Ground truth answer of LTL condition for frames
+    ltl_frame (str): LTL formula
+    number_of_frame (int): Number of frame
+    frames_of_interest (list): List of frames that satisfy LTL condition
+    - [[0]] -> Frame 0 satisfy LTL condition;
+      [[4,5,6,7]] -> Frame 4 to 7 satisfy LTL condition
+      [[0],[4,5,6,7]] -> Frame 0 and Frame 4 to 7 satisfy LTL condition.
+    labels_of_frame: list of labels of frame
+    """
+
+    ground_truth: bool
+    ltl_formula: str
+    proposition: list
+    number_of_frame: int
+    frames_of_interest: Optional[List[List[int]]]
+    labels_of_frames: List[str]
+    images_of_frames: List[np.ndarray] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        """Post init."""
+        self.frames_of_interest = self.group_consecutive_indices(data=self.labels_of_frames)
+
+    def group_consecutive_indices(self, data) -> list:
+        """Group consecutive indices.
+
+        Args:
+        data (list): List of data.
+        """
+        groups = []
+        group = []
+
+        for i, v in enumerate(data):
+            if v is not None:
+                if not group or i - 1 == group[-1]:
+                    group.append(i)
+                else:
+                    groups.append(group)
+                    group = [i]
+        if group:
+            groups.append(group)
+
+        return groups
+
+    def save_image(self, path="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts") -> None:
+        """Save image to path.
+
+        Args:
+        path (str, optional): Path to save image. Defaults to "/opt/Neuro-Symbolic-Video-Frame-Search/artifacts".
+        """
+        from PIL import Image
+
+        for idx, img in enumerate(self.images_of_frames):
+            Image.fromarray(img).save(f"{path}/{idx}.png")
---
+++ ns_vfs/generator/data_generator.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+
+import abc
+import random
+
+from ns_vfs.data.frame import BenchmarkLTLFrame
+from ns_vfs.loader.benchmark_image import BenchmarkImageLoader, Cifar10ImageLoader
+
+
+class DataGenerator(abc.ABC):
+    """Data generator."""
+
+    @abc.abstractmethod
+    def generate(self) -> any:
+        """Generate data."""
+
+
+class BenchmarkVideoGenerator(DataGenerator):
+    """Benchmark video generator."""
+
+    def __init__(
+        self,
+        image_data_loader: BenchmarkImageLoader,
+    ):
+        """Benchmark video generator.
+
+        Args:
+            image_data_loader (BenchmarkImageLoader): Image data loader.
+        """
+        self._data_loader = image_data_loader
+        self.data = self._data_loader.data
+
+        print("d")
+
+    def sample_proposition(self, class_list: list[str]) -> str:
+        """Sample proposition from class list."""
+        return random.sample(class_list, 2)
+
+    def generate(
+        self,
+        max_number_frame: int = 200,
+        number_video_per_set_of_frame: int = 3,
+        increase_rate: int = 50,
+    ) -> any:
+        """Generate data."""
+        number_frame = 25
+        while number_frame <= max_number_frame:
+            for video_idx in range(number_video_per_set_of_frame):
+                proposition = self.sample_proposition(class_list=self.data.unique_labels)
+                ltl_frame = self.ltl_function(
+                    temporal_property="F",
+                    proposition_1=proposition[0],
+                    number_of_frame=number_frame,
+                )
+                (
+                    ltl_frame.labels_of_frames,
+                    ltl_frame.images_of_frames,
+                ) = self.data.sample_image_from_label(
+                    labels=ltl_frame.labels_of_frames, proposition=ltl_frame.proposition
+                )
+
+            number_frame += increase_rate
+
+    def ltl_function(
+        self,
+        proposition_1: str,
+        temporal_property: str,
+        number_of_frame: int,
+        proposition_2: str | None = None,
+        conditional_property: str = "",
+    ) -> BenchmarkLTLFrame:
+        """LTL function.
+
+        Args:
+            proposition_1 (str): Proposition 1.
+            temporal_property (str): Temporal property.
+                - F: eventuallyProperty
+                - G: alwaysProperty
+                - U: untilProperty
+            number_of_frame (int): Number of frame.
+            proposition_2 (str, optional): Proposition 2. Defaults to None.
+            conditional_property (str): Conditional property.
+                - &: andProperty
+                - |: orProperty
+                - !: notProperty.
+        """
+        labels_of_frame = [None] * number_of_frame
+
+        # Single Rule
+        # E.G: F "cat" or G "dog"
+        if proposition_2 is not None:
+            ltl_formula = f'{temporal_property} "{proposition_1}" {conditional_property} "{proposition_2}"'
+        else:
+            ltl_formula = f'{temporal_property} "{proposition_1}"'
+
+        # F "prop1"
+        frames_of_interest = []
+        for _ in range(0, int(number_of_frame / 5)):
+            frame_index = random.randint(0, number_of_frame - 1)
+            labels_of_frame[frame_index] = proposition_1
+            frames_of_interest.append([frame_index])
+        # TODO: Make a false case
+        ltl_frame = BenchmarkLTLFrame(
+            ground_truth=True,
+            ltl_formula=ltl_formula,
+            proposition=[proposition_1, proposition_2],
+            number_of_frame=number_of_frame,
+            frames_of_interest=frames_of_interest,
+            labels_of_frames=labels_of_frame,
+        )
+
+        return ltl_frame
+
+
+if __name__ == "__main__":
+    cifar_dir = (
+        "/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/data/benchmark_image_dataset/cifar-10-batches-py"
+    )
+    cifar_video_generator = BenchmarkVideoGenerator(
+        image_data_loader=Cifar10ImageLoader(cifar_dir_path=cifar_dir)
+    )
+    cifar_video_generator.generate(max_number_frame=50)
+    print("Hello World!")
