49f88e4820ac826bb9920d6bd7bcd5e0d3840849
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-09-30 12:31:18 -0500

    feat: clean a run searcher script

---
+++ run_scripts/run_frame_searcher.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import argparse
+
+from ns_vfs.config.loader import load_config
+from ns_vfs.frame_searcher import FrameSearcher
+from ns_vfs.model.vision.grounding_dino import GroundingDino
+from ns_vfs.model.vision.yolo import Yolo
+from ns_vfs.processor.benchmark_video_processor import BenchmarkVideoFrameProcessor
+from ns_vfs.processor.video_processor import (
+    VideoFrameProcessor,
+)
+from ns_vfs.video_to_automaton import VideotoAutomaton
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cv_model", type=str, default="grounding_dino")
+    parser.add_argument(
+        "--video_processor", type=str, default="regular_video", choices=["regular_video", "benchmark_video"]
+    )
+    parser.add_argument(
+        "--video_path",
+        type=str,
+        default="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/data/nyc_street/nyc_stree_53sec.mp4",
+    )
+    parser.add_argument(
+        "--proposition_set",
+        type=str,
+        default="bicycle, car",
+    )
+    parser.add_argument(
+        "--ltl_formula",
+        type=str,
+        default='P>=0.90 ["car" U "bicycle"]',
+    )
+    parser.add_argument(
+        "--save_annotation",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/frame_search_output",
+    )
+
+    args = parser.parse_args()
+
+    config = load_config()
+
+    if args.video_processor == "regular_video":
+        video_processor = VideoFrameProcessor(
+            video_path=args.video_path,
+            artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+        )
+        proposition_set = args.proposition_set.split(",")
+        ltl_formula = args.ltl_formula
+    else:
+        assert args.video_path.endswith(".pkl"), "Benchmark video path must be a .pkl file"
+        video_processor = BenchmarkVideoFrameProcessor(
+            video_path=args.video_path,
+            artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+        )
+        benchmark_img_frame = video_processor.benchmark_image_frames
+        proposition_set = benchmark_img_frame.proposition
+        ltl_formula = f"P>=0.90 [{benchmark_img_frame.ltl_formula}]"
+
+    if args.cv_model == "grounding_dino":
+        cv_model = GroundingDino(
+            config=config.GROUNDING_DINO,
+            weight_path=config.GROUNDING_DINO.GROUNDING_DINO_CHECKPOINT_PATH,
+            config_path=config.GROUNDING_DINO.GROUNDING_DINO_CONFIG_PATH,
+        )
+    else:
+        cv_model = Yolo(
+            config=config.YOLO,
+            weight_path=config.YOLO.YOLO_CHECKPOINT_PATH,
+        )
+
+    video_automata_builder = VideotoAutomaton(
+        detector=cv_model,
+        video_processor=video_processor,
+        artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+        proposition_set=proposition_set,
+        save_annotation=args.save_annotation,  # TODO: Debug only
+        save_image=False,  # TODO: Debug only
+        ltl_formula=ltl_formula,  # 'P>=0.99 [F "person"]' P>=0.99 [F ("person" U "car")] P>=0.99 [F "person" U "car"]
+    )
+
+    frame_sercher = FrameSearcher(
+        video_automata_builder=video_automata_builder, video_processor=video_processor
+    )
+
+    frame_of_interest = frame_sercher.search()
+    frame_of_interest.save_frames(args.output_dir)
+    # print(frame_of_interest)
+    print("Done!")
