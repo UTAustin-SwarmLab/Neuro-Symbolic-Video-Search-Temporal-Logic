415007cd81bf4903c7c934cddd7b8ade2b571664
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-10-03 10:02:25 -0500

    fix: pull label of interest

--- ns_vfs/loader/benchmark_cifar.py
+++ ns_vfs/loader/benchmark_cifar.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from ns_vfs.data.frame import BenchmarkRawImage
+from ns_vfs.loader import LABEL_OF_INTEREST
 
 from ._base import BenchmarkImageLoader
 
@@ -30,7 +31,6 @@ class Cifar10ImageLoader(BenchmarkImageLoader):
         self.name = "CIFAR10"
         self._cifar_dir_path = Path(cifar_dir_path)
         self._batch_id = f"data_batch_{batch_id}"
-        self.class_labels = ["airplane", "truck", "bird", "cat", "dog", "horse"]  # YOLO labels
         self._original_class_labels = [
             "airplane",
             "automobile",
@@ -43,13 +43,14 @@ class Cifar10ImageLoader(BenchmarkImageLoader):
             "ship",
             "truck",
         ]
+        self.class_labels = [label for label in self._original_class_labels if label in LABEL_OF_INTEREST]
         self.data: BenchmarkRawImage = self.process_data(
             raw_data=self.load_data(data_path=self._cifar_dir_path / self._batch_id)
         )
 
     def process_data(self, raw_data) -> any:
         """Process raw data to BenchmarkRawImage Data Class."""
-        class_labels = list(map(lambda x: self._original_class_labels[x], raw_data[b"labels"]))
+        class_labels = list(map(lambda x: [self._original_class_labels[x]], raw_data[b"labels"]))
         data = raw_data[b"data"].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
         plt.imshow(data[4])
         plt.savefig("test_1.png")
@@ -103,24 +104,8 @@ class Cifar100ImageLoader(BenchmarkImageLoader):
         self._cifar_dir_path = Path(cifar_dir_path) / "train"
         self._raw_data = self.load_data(data_path=self._cifar_dir_path)
         self._original_class_labels = self.get_original_class_labels()
-        self.class_labels = [
-            "bicycle",
-            "motorcycle",
-            "bus",
-            "train",
-            "elephant",
-            "bear",
-            "bottle",
-            "cup",
-            "bowl",
-            "apple",
-            "orange",
-            "chair",
-            "couch",
-            "bed",
-            "mouse",
-            "keyboard",
-            "clock",
+        self.class_labels = self.class_labels = [
+            label for label in self._original_class_labels if label in LABEL_OF_INTEREST
         ]  # YOLO labels
 
         self.data: BenchmarkRawImage = self.process_data(raw_data=self._raw_data)
@@ -133,7 +118,7 @@ class Cifar100ImageLoader(BenchmarkImageLoader):
 
     def process_data(self, raw_data) -> any:
         """Process raw data to BenchmarkRawImage Data Class."""
-        class_labels = list(map(lambda x: self._original_class_labels[x], raw_data[b"fine_labels"]))
+        class_labels = list(map(lambda x: [self._original_class_labels[x]], raw_data[b"fine_labels"]))
         # class_labels:
         # image_idx_1: "label"
         # image_idx_2: "label"
