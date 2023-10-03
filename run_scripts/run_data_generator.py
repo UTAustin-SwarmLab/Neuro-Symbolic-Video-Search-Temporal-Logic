df3351e99b4992c322168e0079be17e2e7e6ee77
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-10-02 19:33:59 -0500

    feat: cifar data loader only for yolo lable

--- run_scripts/run_data_generator.py
+++ run_scripts/run_data_generator.py
@@ -1,12 +1,17 @@
 from ns_vfs.generator.data_generator import BenchmarkVideoGenerator
-from ns_vfs.loader.benchmark_image import Cifar10ImageLoader, ImageNetDataloader
+from ns_vfs.loader.benchmark_image import Cifar10ImageLoader, Cifar100ImageLoader, ImageNetDataloader
 
-DATASET_TYPE = "imagenet"  # "cifar10" or "imagenet"
+DATASET_TYPE = "cifar10"  # "cifar10" or "imagenet"
 
 if __name__ == "__main__":
     if DATASET_TYPE == "cifar10":
         image_dir = "/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/data/benchmark_image_dataset/cifar-10-batches-py"
         image_loader = Cifar10ImageLoader(cifar_dir_path=image_dir)
+    elif DATASET_TYPE == "cifar100":
+        image_dir = (
+            "/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/data/benchmark_image_dataset/cifar-100-python"
+        )
+        image_loader = Cifar100ImageLoader(cifar_dir_path=image_dir)
     elif DATASET_TYPE == "imagenet":
         image_dir = "/store/datasets/ILSVRC"
         image_loader = ImageNetDataloader(imagenet_dir_path=image_dir)
@@ -14,7 +19,7 @@ if __name__ == "__main__":
         image_data_loader=image_loader,
         artificat_dir="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/benchmark_frame_video",
     )
-    video_generator.generate(max_number_frame=100, ltl_logic="prop1 U prop2", save_frames=False)
+    video_generator.generate(max_number_frame=100, ltl_logic="F prop1", save_frames=False)
     # prop1 U prop2
     # F prop1
     # G prop1
