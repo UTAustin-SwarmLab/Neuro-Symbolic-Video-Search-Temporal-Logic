324d61ee7786bac7fdf446cae4455712fd041e6c
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2023-12-05 19:07:28 -0600

    refactor

---
+++ run_scripts/run_nsvs_tl.py
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+import argparse
+
+from ns_vfs.config.loader import load_config
+from ns_vfs.frame_searcher import FrameSearcher
+from ns_vfs.model.vision.clip_model import ClipPerception
+from ns_vfs.model.vision.yolo import Yolo
+from ns_vfs.processor.benchmark_video_processor import (
+    BenchmarkVideoFrameProcessor,
+)
+from ns_vfs.processor.video_processor import (
+    VideoFrameProcessor,
+)
+from ns_vfs.video_to_automaton import VideotoAutomaton
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # - - - - - - - COMMON ARGUMENTS - - - - - - - #
+    parser.add_argument("--cv_model", type=str, default="yolo")
+    parser.add_argument(
+        "--video_processor",
+        type=str,
+        default="tlv_dataset",
+        choices=["regular_video", "tlv_dataset"],
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/_result/frame_search_output",
+    )
+    parser.add_argument(
+        "--video_path",
+        type=str,
+        default="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts/data/nyc_street/nyc_stree_53sec.mp4",
+    )
+    # - - - - - - - - - - - - - - - - - - - - - - - #
+
+    # - - - - - - - REGULAR VIDEO ONLY - - - - - - #
+    parser.add_argument(
+        "--proposition_set",
+        type=str,
+        default="bicycle,car",
+        help="No space between propositions, separated by comma",
+    )
+    parser.add_argument(
+        "--ltl_formula",
+        type=str,
+        default='P>=0.90 ["car" U "bicycle"]',
+    )
+    # - - - - - - - - - - - - - - - - - - - - - - - #
+
+    # - - - - - - - - CHOOSE CAREFULLY - - - - - - - #
+    parser.add_argument(
+        "--save_annotation",
+        type=bool,
+        default=False,
+    )
+
+    parser.add_argument(
+        "--manual_confidence_probability",
+        type=str,
+        default=None,
+    )
+    # - - - - - - - - - - - - - - - - - - - - - - - #
+
+    args = parser.parse_args()
+
+    config = load_config()
+
+    if args.video_processor == "regular_video":
+        video_processor = VideoFrameProcessor(
+            video_path=args.video_path,
+            artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+        )
+        proposition_set = args.proposition_set.split(",")
+        ltl_formula = args.ltl_formula
+    else:
+        assert args.video_path.endswith(
+            ".pkl"
+        ), "Temporal Logic Video dataset must be a .pkl file"
+        manual_confidence_probability = args.manual_confidence_probability
+        video_processor = BenchmarkVideoFrameProcessor(
+            video_path=args.video_path,
+            artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+            manual_confidence_probability=manual_confidence_probability,
+        )
+        benchmark_img_frame = video_processor.benchmark_image_frames
+        proposition_set = benchmark_img_frame.proposition
+        ltl_formula = f"P>=0.90 [{benchmark_img_frame.ltl_formula}]"
+
+    if args.cv_model == "yolo":
+        cv_model = Yolo(
+            config=config.YOLO,
+            weight_path=config.YOLO.YOLO_CHECKPOINT_PATH,
+        )
+
+    elif args.cv_model == "clip":
+        cv_model = ClipPerception(config=config, weight_path=None)
+
+    else:
+        cv_model = {
+            "yolo": Yolo(
+                config=config.YOLO,
+                weight_path=config.YOLO.YOLO_CHECKPOINT_PATH,
+            ),
+            "clip": ClipPerception(config=config, weight_path=None),
+        }
+
+    video_automata_builder = VideotoAutomaton(
+        detector=cv_model,
+        video_processor=video_processor,
+        artifact_dir=config.VERSION_AND_PATH.ARTIFACTS_PATH,
+        proposition_set=proposition_set,
+        save_annotation=args.save_annotation,  # TODO: Debug only
+        save_image=False,  # TODO: Debug only
+        ltl_formula=ltl_formula,
+    )
+
+    frame_sercher = FrameSearcher(
+        video_automata_builder=video_automata_builder,
+        video_processor=video_processor,
+    )
+
+    frame_of_interest = frame_sercher.search()
+    frame_of_interest.save_frames(args.output_dir)
+
+    print("Done!")
