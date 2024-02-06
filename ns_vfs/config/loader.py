8f348632076cd379002f9579fddcc6b29e36eb5b
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-02-05 19:45:16 -0600

    clean config

--- ns_vfs/config/loader.py
+++ ns_vfs/config/loader.py
@@ -15,35 +15,11 @@ def load_config():
         "weights",
         "sam_vit_h_4b8939.pth",
     )
-    config.GROUNDING_DINO.GROUNDING_DINO_CHECKPOINT_PATH = os.path.join(
-        config.VERSION_AND_PATH.ARTIFACTS_PATH,
-        "weights",
-        "groundingdino_swinb_cogcoor.pth",
-    )
-    config.GROUNDING_DINO.GROUNDING_DINO_CONFIG_PATH = os.path.join(
-        config.VERSION_AND_PATH.ARTIFACTS_PATH,
-        "weights",
-        "GroundingDINO_SwinB_cfg.py",
-    )
+
     config.YOLO.YOLO_CHECKPOINT_PATH = os.path.join(
         config.VERSION_AND_PATH.ARTIFACTS_PATH,
         "weights",
         "yolov8x.pt",
-    )
-    config.YOLOX.YOLOX_CHECKPOINT_PATH = os.path.join(
-        config.VERSION_AND_PATH.ARTIFACTS_PATH,
-        "weights",
-        "yolox_x.pth",
-    )
-    config.MMDETECTION.MMDETECTION_CONFIG_PATH = os.path.join(
-        config.VERSION_AND_PATH.ARTIFACTS_PATH,
-        "weights",
-        "mask-rcnn_x101-64x4d_fpn_ms-poly_3x_coco.py",
-    )
-    config.MMDETECTION.MMDETECTION_CHECKPOINT_PATH = os.path.join(
-        config.VERSION_AND_PATH.ARTIFACTS_PATH,
-        "weights",
-        "mask_rcnn_x101_64x4d_fpn_mstrain-poly_3x_coco_20210526_120447-c376f129.pth",
     )
 
     return config
