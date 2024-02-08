c66b3aa343b46c0f1eb7e05ee630b47fe1eded2d
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-02-07 20:59:19 -0600

    update run scripts

--- run_scripts/run_video_to_automata_with_tlv_dataset.py
+++ run_scripts/run_video_to_automata_with_tlv_dataset.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-import cv2
-
 from ns_vfs.automaton.probabilistic import ProbabilisticAutomaton
+from ns_vfs.common.utility import save_image
 from ns_vfs.config.loader import load_config
 from ns_vfs.data.frame import Frame, FramesofInterest
 from ns_vfs.model.vision.object_detection.clip_model import ClipPerception
 from ns_vfs.model.vision.object_detection.yolo import Yolo
 from ns_vfs.model_checker.stormpy import StormModelChecker
 from ns_vfs.percepter.multi_vision_percepter import MultiVisionPercepter
-from ns_vfs.processor.video.real_video import RealVideoProcessor
+from ns_vfs.processor.tlv_dataset.tlv_dataset_processor import (
+    TLVDatasetProcessor,
+)
 from ns_vfs.validator import FrameValidator
 
 
@@ -17,11 +18,12 @@ def main():
     # Given Propositions
     # LTL Formula
     config = load_config()
-    video_path = "/opt/Neuro-Symbolic-Video-Frame-Search/sample_data/titanic_scene.mp4"  # Replace with your video path
-    ltl_formula = 'P>=0.80 [F "person"]'
-    proposition_set = ["person", "apple"]
+    video_path = '/opt/Neuro-Symbolic-Video-Frame-Search/sample_data/ouputs/benchmark_ImageNet2017-1K_ltl_"stop_sign" U "sheep"_5_0.pkl'  # Replace with your video path
+
     # Initialize the video processor
-    processor = RealVideoProcessor(video_path, frame_duration_sec=1, frame_scale=2)
+    processor = TLVDatasetProcessor(video_path)
+    ltl_formula = f"P>=0.80 [{processor.ltl_formula}]"
+    proposition_set = processor.proposition_set
     cv_model = {
         "yolo": Yolo(
             weight_path=config.YOLO.YOLO_CHECKPOINT_PATH,
@@ -40,6 +42,7 @@ def main():
 
     frame_idx = 0
 
+    print(processor.labels_of_frames)
     while True:
         # Get the next frame
         frame_img = processor.get_next_frame()
@@ -48,15 +51,17 @@ def main():
         detected_objects: list = percepter.perceive(
             image=frame_img, object_of_interest=proposition_set
         )
-        detected_activity = None
+        activity_of_interest = None
 
         frame = Frame(
             frame_idx=frame_idx,
             timestamp=processor.current_frame_index,
             frame_image=frame_img,
-            object_detection=detected_objects,
-            activity_detection=detected_activity,
+            object_of_interest=detected_objects,
+            activity_of_interest=activity_of_interest,
         )
+        save_image(image=frame_img, file_path="test1.png")
+
         # 1. frame validation
         if frame_validator.validate_frame(frame=frame):
             # 2. dynamic automaton construction
@@ -74,15 +79,11 @@ def main():
                 frame_of_interest.flush_frame_buffer()
                 automaton.reset()
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
         frame_idx += 1
 
-    # Release resources
-    cv2.destroyAllWindows()
     print(frame_of_interest.foi_list)
+    print(processor.frames_of_interest)
+    print(processor.labels_of_frames)
 
 
 if __name__ == "__main__":
