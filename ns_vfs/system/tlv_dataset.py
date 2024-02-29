3ed63e86586b35f41fa9419cb3f7b7290efeb7bf
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-02-28 18:43:04 -0600

    pull main

---
+++ ns_vfs/system/tlv_dataset.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+from omegaconf import DictConfig
+
+from ns_vfs.automaton._base import Automaton
+from ns_vfs.data.frame import Frame, FramesofInterest
+from ns_vfs.model.vision.object_detection.dummy import DummyVisionModel
+from ns_vfs.model_checker.stormpy import StormModelChecker
+from ns_vfs.percepter._base import VisionPercepter
+from ns_vfs.processor._base_video_processor import BaseVideoProcessor
+from ns_vfs.processor.tlv_dataset.tlv_dataset_processor import (
+    TLVDatasetProcessor,
+)
+from ns_vfs.system.node import Node
+from ns_vfs.validator import FrameValidator
+
+
+class NSVSNodeTLVDataset(Node):
+    def __init__(
+        self,
+        video_processor: BaseVideoProcessor,
+        vision_percepter: VisionPercepter,
+        automaton: Automaton,
+        ltl_formula: str,
+        proposition_set: str,
+        ns_vfs_system_cfg: DictConfig,
+    ) -> None:
+        self.video_processor = video_processor
+        if isinstance(video_processor, TLVDatasetProcessor):
+            self.ltl_formula = f"Pmin=? [{video_processor.ltl_formula}]"
+            self.proposition_set = video_processor.proposition_set
+        else:
+            self.ltl_formula = ltl_formula
+            self.proposition_set = proposition_set
+        self.vision_percepter = vision_percepter
+
+        self.frame_validator = FrameValidator(ltl_formula=self.ltl_formula)
+        self.automaton = automaton
+        self.automaton.set_up(proposition_set=self.proposition_set)
+        self.model_checker = StormModelChecker(
+            proposition_set=self.proposition_set, ltl_formula=self.ltl_formula
+        )
+        self.frame_of_interest = FramesofInterest(ltl_formula=self.ltl_formula)
+
+        self.ns_vfs_system_cfg = ns_vfs_system_cfg
+        self.frame_idx = 0
+
+    def start(self) -> None:
+        while True:
+            frame_img = self.video_processor.get_next_frame()
+            if frame_img is None:
+                break  # No more frames or end of video
+            if isinstance(self.vision_percepter.cv_model, DummyVisionModel):
+                ground_truth_object = (
+                    self.video_processor.get_ground_truth_label(
+                        frame_idx=self.frame_idx
+                    )
+                )
+            else:
+                ground_truth_object = None
+
+            detected_objects: list = self.vision_percepter.perceive(
+                image=frame_img,
+                object_of_interest=self.proposition_set,
+                ground_truth_object=ground_truth_object,
+            )
+            activity_of_interest = None
+
+            frame = Frame(
+                frame_idx=self.frame_idx,
+                timestamp=self.video_processor.current_frame_index,
+                frame_image=frame_img,
+                object_of_interest=detected_objects,
+                activity_of_interest=activity_of_interest,
+            )
+            self.frame_idx += 1
+
+            # 1. frame validation
+            if self.frame_validator.validate_frame(frame=frame):
+                # 2. dynamic automaton construction
+                self.automaton.add_frame_to_automaton(frame=frame)
+                self.frame_of_interest.frame_buffer.append(frame)
+                # 3. model checking
+                model_checking_result = self.model_checker.check_automaton(
+                    transitions=self.automaton.transitions,
+                    states=self.automaton.states,
+                    verbose=self.ns_vfs_system_cfg.model_checker.verbose,
+                    is_filter=self.ns_vfs_system_cfg.model_checker.is_filter,
+                )
+                if model_checking_result:
+                    # specification satisfied
+                    self.frame_of_interest.flush_frame_buffer()
+                    self.automaton.reset()
+
+        print(self.frame_of_interest.foi_list)
+        print(self.video_processor.frames_of_interest)
+        print(self.video_processor.labels_of_frames)
+
+    def stop(self) -> None:
+        print("NSVS Node stopped")
---
+++ tests/automaton/test_probalistic.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pytest
+
+from ns_vfs.automaton.probabilistic import ProbabilisticAutomaton
+from ns_vfs.data.frame import Frame
+
+
+@pytest.fixture
+def probabilistic_automaton_with_initial_state():
+    automaton = ProbabilisticAutomaton(
+        include_initial_state=True, proposition_set=["object 1", "object 0"]
+    )
+    automaton.set_up(proposition_set=["object 1", "object 0"])
+    return automaton
+
+
+@pytest.fixture
+def probabilistic_automaton_without_initial_state():
+    automaton = ProbabilisticAutomaton(
+        include_initial_state=False, proposition_set=["object 1", "object 0"]
+    )
+    automaton.set_up(proposition_set=["object 1", "object 0"])
+    return automaton
+
+
+def test_create_proposition_combinations(
+    probabilistic_automaton_without_initial_state,
+):
+    automaton = probabilistic_automaton_without_initial_state
+    assert automaton._create_proposition_combinations(
+        len(automaton.proposition_set)
+    ) == ["TT", "TF", "FT", "FF"]
+
+
+def test_update_probability_of_propositions(
+    probabilistic_automaton_without_initial_state,
+    frame_with_and_without_detected_object,
+):
+    frame = frame_with_and_without_detected_object
+    automaton = probabilistic_automaton_without_initial_state
+    automaton._update_probability_of_propositions(frame)
+    assert automaton.probability_of_propositions == [[1.0], [0.0]]
+
+
+def test_probabilistic_automaton_with_initial_state(
+    probabilistic_automaton_with_initial_state,
+    frame_with_and_without_detected_object,
+):
+    # test case 1: init test
+    # test case 2: rest test
+    frame = frame_with_and_without_detected_object
+    automaton = probabilistic_automaton_with_initial_state
+    assert len(automaton.previous_states) == 1
+    assert len(automaton.states) == 1
+    assert len(automaton.transitions) == 0
+    assert automaton.proposition_set == ["object 1", "object 0"]
+    assert automaton.probability_of_propositions == [[], []]
+
+    automaton.add_frame_to_automaton(frame)
+
+    automaton.reset()
+    assert len(automaton.previous_states) == 1
+    assert len(automaton.states) == 1
+    assert len(automaton.transitions) == 0
+    assert automaton.probability_of_propositions == [[], []]
+
+
+def test_probabilistic_automaton_without_initial_state(
+    probabilistic_automaton_without_initial_state,
+):
+    automaton = probabilistic_automaton_without_initial_state
+    assert len(automaton.previous_states) == 0
+    assert len(automaton.states) == 0
+    assert len(automaton.transitions) == 0
+    assert automaton.proposition_set == ["object 1", "object 0"]
+    assert automaton.probability_of_propositions == [[], []]
+
+
+def test_add_frame_to_automaton_1(
+    prob_1_detected_object,
+    prob_0_8_detected_object,
+):
+    # prob 1 and prob 0.8 are detected
+    automaton_no_init = ProbabilisticAutomaton(
+        include_initial_state=False,
+    )
+    automaton_no_init.set_up(
+        proposition_set=[
+            prob_1_detected_object.name,
+            prob_0_8_detected_object.name,
+        ]
+    )
+    frame_1 = Frame(
+        frame_idx=0,
+        timestamp=0,
+        frame_image=np.zeros((100, 100, 3), dtype=np.uint8),
+        annotated_image={},
+        object_of_interest={
+            prob_1_detected_object.name: prob_1_detected_object,
+            prob_0_8_detected_object.name: prob_0_8_detected_object,
+        },
+        activity_of_interest=None,
+    )
+    automaton_no_init.add_frame_to_automaton(frame_1)
+    first_state_prop = automaton_no_init.transitions[0][2]
+    second_state_prop = automaton_no_init.transitions[1][2]
+    assert len(automaton_no_init.transitions) == 2
+    assert automaton_no_init.transitions[0][2] == 0.8
+    assert automaton_no_init.transitions[1][2] == 0.2
+    assert first_state_prop + second_state_prop == 1.0
+
+
+def test_add_frame_to_automaton_2(
+    prob_0_8_detected_object,
+    prob_0_3_detected_object,
+):
+    # test case 1: prob 0.8 and prob 0.3 are detected
+    # test case 2: add frame twice
+    # test case 3: reset
+    automaton_no_init = ProbabilisticAutomaton(
+        include_initial_state=False,
+    )
+    automaton_no_init.set_up(
+        proposition_set=[
+            prob_0_3_detected_object.name,
+            prob_0_8_detected_object.name,
+        ]
+    )
+    frame_2 = Frame(
+        frame_idx=0,
+        timestamp=0,
+        frame_image=np.zeros((100, 100, 3), dtype=np.uint8),
+        annotated_image={},
+        object_of_interest={
+            prob_0_3_detected_object.name: prob_0_3_detected_object,
+            prob_0_8_detected_object.name: prob_0_8_detected_object,
+        },
+        activity_of_interest=None,
+    )
+
+    automaton_no_init.add_frame_to_automaton(frame_2)
+
+    assert len(automaton_no_init.transitions) == 4
+    assert automaton_no_init.transitions[0][2] == 0.24
+    assert automaton_no_init.transitions[1][2] == 0.06
+    assert automaton_no_init.transitions[2][2] == 0.56
+    assert automaton_no_init.transitions[3][2] == 0.14
+
+    automaton_no_init.add_frame_to_automaton(frame_2)
+    print(automaton_no_init.states)
+    print(automaton_no_init.previous_states)
+    print(automaton_no_init.transitions)
+
+    assert len(automaton_no_init.transitions) == 20
+
+    automaton_no_init.reset()
+    assert len(automaton_no_init.previous_states) == 0
+    assert len(automaton_no_init.states) == 0
+    assert len(automaton_no_init.transitions) == 0
+    assert automaton_no_init.probability_of_propositions == [[], []]
