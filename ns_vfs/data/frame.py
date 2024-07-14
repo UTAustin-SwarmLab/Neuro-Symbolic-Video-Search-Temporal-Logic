effd80c42024fa9610cea60c6b8ad8ee5e599cef
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-07-14 10:08:35 -0500

    TLV dataclass

--- ns_vfs/data/frame.py
+++ ns_vfs/data/frame.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
 import dataclasses  # noqa: D100
+import random
 from pathlib import Path
 from typing import Dict, List, Optional
 
 import cv2
 import numpy as np
+import torch
 from PIL import Image
 
+from ns_vfs.common.frame_grouping import combine_consecutive_lists
 from ns_vfs.common.utility import (
     get_file_or_dir_with_datetime,
 )
@@ -155,3 +158,157 @@ class FramesofInterest:
                     )
             except:  # noqa: E722
                 pass
+
+
+@dataclasses.dataclass
+class BenchmarkRawImage:
+    """Benchmark image frame class."""
+
+    unique_labels: list
+    labels: List[List[str]]
+    images: List[np.ndarray]
+
+    def sample_image_from_label(
+        self, labels: list, proposition: list
+    ) -> np.ndarray:
+        """Sample image from label."""
+        image_of_frame = []
+        img_to_label = {}
+        img_to_label_list = {}
+        for prop in proposition:
+            # img_to_label[prop] = [i for i, value in enumerate(self.labels) if value == prop]
+            img_to_label[prop] = [
+                i for i, value in enumerate(self.labels) if prop in value
+            ]
+        # img_to_label_list[tuple(sorted(proposition))] = [
+        #     i for i, value in enumerate(self.labels) if all(prop in value for prop in proposition)
+        # ]
+
+        label_idx = 0
+        for label in labels:
+            if label is None:
+                while True:
+                    random_idx = random.randrange(
+                        len(self.images)
+                    )  # pick one random image with idx
+                    val = []
+                    for single_label in self.labels[
+                        random_idx
+                    ]:  # go over all labels of the image
+                        if single_label in proposition:
+                            val.append(True)
+                    if True not in val:
+                        labels[label_idx] = single_label
+                        image_of_frame.append(self.images[random_idx])
+                        break
+            else:
+                # lable available - just get the image
+                if isinstance(label, str):
+                    # one label in the frame
+                    random_idx = random.choice(img_to_label[label])
+                    # plt.imshow(self.images[random_idx])
+                    # plt.axis("off")
+                    # plt.savefig("data_loader_sample_image.png")
+                    image_of_frame.append(self.images[random_idx])
+                elif isinstance(label, list):
+                    img_to_label_list[tuple(sorted(label))] = [
+                        i
+                        for i, value in enumerate(self.labels)
+                        if all(prop in value for prop in label)
+                    ]
+                    random_idx = random.choice(
+                        img_to_label_list[tuple(sorted(label))]
+                    )
+                    image_of_frame.append(self.images[random_idx])
+            label_idx += 1
+        return labels, image_of_frame
+
+
+@dataclasses.dataclass
+class BenchmarkRawImageDataset:
+    """Benchmark image frame class with a torchvision dataset for large datasets."""
+
+    unique_labels: list
+    labels: List[List[str]]
+    images: torch.utils.data.Dataset
+
+    def sample_image_from_label(
+        self, labels: list, proposition: list
+    ) -> np.ndarray:
+        """Sample image from label."""
+        image_of_frame = []
+        img_to_label = {}
+        for prop in proposition:
+            # img_to_label[prop] = [i for i, value in enumerate(self.labels) if value == prop]
+            img_to_label[prop] = [
+                i for i, value in enumerate(self.labels) if prop in value
+            ]
+
+        label_idx = 0
+        for label in labels:
+            if label is None:
+                while True:
+                    random_idx = random.randrange(len(self.images))
+                    if self.labels[random_idx] not in proposition:
+                        break
+                labels[label_idx] = self.labels[random_idx]
+                image_of_frame.append(self.images[random_idx][0])
+            else:
+                random_idx = random.choice(img_to_label[label])
+                image_of_frame.append(self.images[random_idx][0])
+
+            label_idx += 1
+        return labels, image_of_frame
+
+
+@dataclasses.dataclass
+class BenchmarkLTLFrame:
+    """Benchmark image frame class.
+
+    ground_truth (bool): Ground truth answer of LTL condition for frames
+    ltl_frame (str): LTL formula
+    number_of_frame (int): Number of frame
+    frames_of_interest (list): List of frames that satisfy LTL condition
+    - [[0]] -> Frame 0 satisfy LTL condition;
+      [[4,5,6,7]] -> Frame 4 to 7 satisfy LTL condition
+      [[0],[4,5,6,7]] -> Frame 0 and Frame 4 to 7 satisfy LTL condition.
+    labels_of_frame: list of labels of frame
+    """
+
+    ground_truth: bool
+    ltl_formula: str
+    proposition: list
+    number_of_frame: int
+    frames_of_interest: Optional[List[List[int]]]
+    labels_of_frames: List[str]
+    images_of_frames: List[np.ndarray] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        """Post init."""
+        self.frames_of_interest = combine_consecutive_lists(
+            data=self.frames_of_interest
+        )
+
+    def save_frames(
+        self, path="/opt/Neuro-Symbolic-Video-Frame-Search/artifacts"
+    ) -> None:
+        """Save image to path.
+
+        Args:
+        path (str, optional): Path to save image.
+        """
+        from PIL import Image
+
+        for idx, img in enumerate(self.images_of_frames):
+            Image.fromarray(img).save(f"{path}/{idx}.png")
+
+    def save(
+        self,
+        save_path: str = "/opt/Neuro-Symbolic-Video-Frame-Search/artifacts",
+    ) -> None:
+        """Save the current instance to a pickle file."""
+        import pickle
+
+        """Save the current instance to a pickle file."""
+        with open(save_path, "wb") as f:
+            pickle.dump(self, f)
